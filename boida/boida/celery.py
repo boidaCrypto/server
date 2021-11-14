d4a52dc0ea1319517b94e8e6617a492148901c3b
BK <57130081+qudrhks258@users.noreply.github.com>
2021-11-14 19:03:00 +0900

    update

--- boida/boida/celery.py
+++ boida/boida/celery.py
@@ -5,7 +5,7 @@ from celery import Celery
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", 'boida.settings')
 app = Celery('boida')
-app.config_from_object('django.conf:settings', namespace="celery")
+app.config_from_object('django.conf:settings', namespace="CELERY")
 app.autodiscover_tasks()
 
 @app.task(bind=True)
--- boida/boida/settings.py
+++ boida/boida/settings.py
@@ -219,7 +219,7 @@ SIMPLE_JWT = {
 
 # celery 연동
 CELERY_ALWAYS_EAGER = True
-CELERY_BROKER_URL = 'redis://127.0.0.1:6379'
+CELERY_BROKER_URL = 'redis://localhost:6379'
 # CELERY_RESULT_BACKEND = 'redis://localhost:6379'
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_TASK_SERIALIZER = 'json'

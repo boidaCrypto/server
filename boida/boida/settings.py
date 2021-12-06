e47111212734fe8646bea6e91b031ee2d6ea1988
BK <57130081+qudrhks258@users.noreply.github.com>
2021-12-06 15:13:26 +0900

    update

--- boida/boida/settings.py
+++ boida/boida/settings.py
@@ -234,8 +234,8 @@ REST_USE_JWT = True
 from datetime import timedelta
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=1),
-    'REFRESH_TOKEN_LIFETIME': timedelta(minutes=1),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+    'REFRESH_TOKEN_LIFETIME': timedelta(minutes=10),
     'ROTATE_REFRESH_TOKENS': False,
     'BLACKLIST_AFTER_ROTATION': True,
 }
